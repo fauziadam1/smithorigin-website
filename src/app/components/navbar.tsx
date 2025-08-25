d17bb136e72b8aaa5cd3dc5133c6704ced7f9aa7
Fauzi Adam <fauziadam0811@gmail.com>
2025-08-25 20:27:30 +0700

    update responsive

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -42,56 +42,58 @@ export default function Header() {
     }, []);
 
     return (
-        <nav className={`fixed z-[1000] w-full mx-auto px-25 py-4 transition-colors duration-300 flex items-center justify-between ${navbarScrolled ? "bg-white text-foreground border-b-1 border-[#CCCC]" : "bg-transparent"}`}>
-            <Link href="#" className="flex items-center">
-                <Image src="/Logo.png" alt="Logo" width={60} height={60}/>
-                <h1 className={clsx("font-[700] text-[15px] leading-5 transition-colors", navbarScrolled ? "text-black" : "text-white")}>SMITH <br /> ORIGIN</h1>
-            </Link>
-            <div className="flex items-center gap-6">
-                <NavigationMenu viewport={false}>
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild className={clsx(navigationMenuTriggerStyle(), navbarScrolled ? "text-black" : "text-white")}>
-                                <Link href="/docs">Home</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild className={clsx(navigationMenuTriggerStyle(), navbarScrolled ? "text-black" : "text-white")}>
-                                <Link href="/docs">Forum</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger className={clsx(navbarScrolled ? "text-black" : "text-white")}>Category</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid w-[100px] gap-4">
-                                    <li>
-                                        <NavigationMenuContentLink asChild>
-                                            <Link href="#">Keyboard</Link>
-                                        </NavigationMenuContentLink>
-                                        <NavigationMenuContentLink asChild>
-                                            <Link href="#">Mouse</Link>
-                                        </NavigationMenuContentLink>
-                                        <NavigationMenuContentLink asChild>
-                                            <Link href="#">Speaker</Link>
-                                        </NavigationMenuContentLink>
-                                        <NavigationMenuContentLink asChild>
-                                            <Link href="#">Deskmat</Link>
-                                        </NavigationMenuContentLink>
-                                    </li>
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-                <form action="POST" className={clsx("bg-background/30 py-3 px-5 text-[12px] text-background flex items-center gap-3 rounded-full mr-2", navbarScrolled ? "border-1 border-[#CCCCC]" : "border-none")}>
-                    <label htmlFor="search">
-                        <SearchIcon className={clsx("w-[20px] h-[20px]", navbarScrolled ? "text-foreground" : "text-background")} />
-                    </label>
-                    <input type="text" placeholder="Search..." className={clsx("outline-none border-none", navbarScrolled ? "text-foreground placeholder-foreground" : "text-background placeholder-background")} id="search" />
-                </form>
-                <Button className="bg-button text-white font-[500] py-6" radius="full" startContent={<SignIn />}>
-                    Sign In
-                </Button>
+        <nav className={`fixed z-[1000] w-full mx-auto py-6 transition-colors duration-300 ${navbarScrolled ? "bg-white text-foreground border-b-1 border-[#CCCC]" : "bg-transparent"}`}>
+            <div className="container mx-auto px-10 flex items-center justify-between">
+                <Link href="#" className="flex items-center gap-2">
+                    <Image src="/Logo.png" alt="Logo" width={50} height={50} />
+                    <h1 className={clsx("font-[700] text-[15px] leading-5 transition-colors", navbarScrolled ? "text-black" : "text-white")}>SMITH <br /> ORIGIN</h1>
+                </Link>
+                <div className="flex items-center gap-6">
+                    <NavigationMenu viewport={false}>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink asChild className={clsx(navigationMenuTriggerStyle(), navbarScrolled ? "text-black" : "text-white")}>
+                                    <Link href="/docs">Home</Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink asChild className={clsx(navigationMenuTriggerStyle(), navbarScrolled ? "text-black" : "text-white")}>
+                                    <Link href="/docs">Forum</Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger className={clsx(navbarScrolled ? "text-black" : "text-white")}>Category</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <ul className="grid w-[100px] gap-4">
+                                        <li>
+                                            <NavigationMenuContentLink asChild>
+                                                <Link href="#">Keyboard</Link>
+                                            </NavigationMenuContentLink>
+                                            <NavigationMenuContentLink asChild>
+                                                <Link href="#">Mouse</Link>
+                                            </NavigationMenuContentLink>
+                                            <NavigationMenuContentLink asChild>
+                                                <Link href="#">Speaker</Link>
+                                            </NavigationMenuContentLink>
+                                            <NavigationMenuContentLink asChild>
+                                                <Link href="#">Deskmat</Link>
+                                            </NavigationMenuContentLink>
+                                        </li>
+                                    </ul>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                    <form action="POST" className={clsx("bg-background/30 py-3 px-5 text-[12px] text-background flex items-center gap-3 rounded-full mr-2", navbarScrolled ? "border-1 border-[#CCCCC]" : "border-none")}>
+                        <label htmlFor="search">
+                            <SearchIcon className={clsx("w-[20px] h-[20px]", navbarScrolled ? "text-foreground" : "text-background")} />
+                        </label>
+                        <input type="text" placeholder="Search..." className={clsx("outline-none border-none", navbarScrolled ? "text-foreground placeholder-foreground" : "text-background placeholder-background")} id="search" />
+                    </form>
+                    <Button className="bg-button text-white font-[500] py-6" radius="full" startContent={<SignIn />}>
+                        Sign In
+                    </Button>
+                </div>
             </div>
         </nav >
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,84 @@
 import { Button } from "@heroui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full absolute left-0">
-      <section className="w-full h-screen bg-cover bg-center bg-hero pt-20">
-        <div className="container mx-auto px-4 flex items-center justify-between">
+    <div className="w-full absolute left-0 space-x-0">
+      <section className="w-full h-screen bg-cover bg-center bg-hero pt-15">
+        <div className="container mx-auto px-10 flex items-center justify-between">
           <div className="flex flex-col items-start justify-center h-[80vh] text-white gap-5">
-            <h1 className="text-button text-7xl font-[900] leading-15">
+            <h1 className="text-button font-[900] text-3xl xl:text-7xl sm:text-6xl">
               LEVEL UP <br />
-              <span className="text-background text-5xl font-[700]">YOUR GAMING GEAR</span>
+              <span className="text-background text-5xl xl:text-5xl sm:text-5xl font-[700]">YOUR GAMING GEAR</span>
             </h1>
-            <p className="max-w-lg">
+            <p className="max-w-md md:max-w-lg md:text-sm sm:text-[13px] sm:max-w-sm">
               Toko penyedia gaming gear nomor 1 di Indonesia. Ikuti terus perkembangan dan produk terbaru dari kami.
             </p>
-            <Button className="bg-button text-white font-[500] py-6 px-7" radius="full">
+            <Button className="bg-button text-white font-[500] md:py-6 md:px-7 md:text-[15px] sm:" radius="full">
               Start Shopping
             </Button>
           </div>
-          <Image src="/Logo.png" alt="Logo" width={380} height={380} className="translate-x-10"/>
+          <Image src="/Logo.png" alt="Logo" width={330} height={330} className="hidden lg:block xl:-translate-x-30 xl:w-80 lg:w-70"/>
         </div>
       </section>
-      <section className="bg-background w-full h-screen">
-
+      <section className="bg-background w-full h-screen ">
+        <div className="container mx-auto px-10">
+          <div className="-translate-y-20">
+            <Card className="">
+              <CardHeader>
+                <CardTitle className="text-center pb-4">
+                  <h1 className="text-lg md:text-xl">Brand We Have</h1>
+                </CardTitle>
+                <CardContent className="grid grid-cols-5 place-items-center gap-5">
+                  <Image
+                    src="/Logo Aula.png"
+                    alt="Brand Logo"
+                    width={150}
+                    height={150}
+                    className="w-12 h-12 sm:w-16 sm:h-16 md:w-40 md:h-40 object-contain"
+                  />
+                  <Image
+                    src="/Logo Leobog.png"
+                    alt="Brand Logo"
+                    width={150}
+                    height={150}
+                    className="w-12 h-12 sm:w-16 sm:h-16 md:w-40 md:h-40 object-contain"
+                  />
+                  <Image
+                    src="/Logo Vortex.png"
+                    alt="Brand Logo"
+                    width={150}
+                    height={150}
+                    className="w-12 h-12 sm:w-16 sm:h-16 md:w-40 md:h-40 object-contain"
+                  />
+                  <Image
+                    src="/Logo WK.png"
+                    alt="Brand Logo"
+                    width={150}
+                    height={150}
+                    className="w-12 h-12 sm:w-16 sm:h-16 md:w-40 md:h-40 object-contain"
+                  />
+                  <Image
+                    src="/Logo MCHOSE.png"
+                    alt="Brand Logo"
+                    width={150}
+                    height={150}
+                    className="w-12 h-12 sm:w-16 sm:h-16 md:w-40 md:h-40 object-contain"
+                  />
+                </CardContent>
+              </CardHeader>
+            </Card>
+          </div>
+        </div>
       </section>
     </div>
   );
